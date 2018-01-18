c6acf10b014607f1541a398206208e568a4714e4
Raimon Grau <raimonster@gmail.com>
2018-01-18 14:46:08 +0000

    Rename project to git-msg-prefix

--- git-msg-prefix.el
+++ git-msg-prefix.el
@@ -1,11 +1,11 @@
-;;; commit-msg-prefix.el --- Insert commit message prefix (issue number)  -*- lexical-binding: t; -*-
+;;; git-msg-prefix.el --- Insert commit message prefix (issue number)  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2017  Raimon Grau
+;; Copyright (C) 2017, 2018 Raimon Grau
 
 ;; Author: Raimon Grau <raimonster@gmail.com>
 ;; Keywords: vc, tools
 
-;; URL: http://github.com/kidd/commit-msg-prefix.el
+;; URL: http://github.com/kidd/git-msg-prefix.el
 ;; Version: 0.1.0
 ;; Package-Requires: ((emacs "24") (s "1.10.0") (dash "2.9.0"))
 
@@ -32,42 +32,42 @@
 ;; The default command presents a searchable list of the previous
 ;; commits (more recent first), and lets you select the one you want.
 ;; This list of candidates is configurable via
-;; `commit-msg-prefix-log-command`.  Once selected, the relevant part
+;; `git-msg-prefix-log-command`.  Once selected, the relevant part
 ;; of the commit line will be extracted from the choosen candidate
-;; via the regex in `commit-msg-prefix-regex', and the matched text
+;; via the regex in `git-msg-prefix-regex', and the matched text
 ;; will be inserted in the current buffer.
 
 ;; example configurations with use-package:
 
-;; (use-package commit-msg-prefix
+;; (use-package git-msg-prefix
 ;;   :ensure t
 ;;   :config
-;;   (setq commit-msg-prefix-log-flags " --since='1 week ago' "
-;;         commit-msg-prefix-input-method 'helm-comp-read)
-;;   (add-hook 'git-commit-mode-hook 'commit-msg-prefix))
+;;   (setq git-msg-prefix-log-flags " --since='1 week ago' "
+;;         git-msg-prefix-input-method 'helm-comp-read)
+;;   (add-hook 'git-commit-mode-hook 'git-msg-prefix))
 
 ;; bare configuration:
 ;;
-;; (add-hook 'git-commit-mode-hook 'commit-msg-prefix)
-;; (setq commit-msg-prefix-input-method 'helm-comp-read)
+;; (add-hook 'git-commit-mode-hook 'git-msg-prefix)
+;; (setq git-msg-prefix-input-method 'helm-comp-read)
 
 ;; Otherwise, add a keybinding to that function or run it manually
 ;; from the minibuffer.
 ;;   (local-set-key
 ;;    (kbd "C-c i")
-;;    'commit-msg-prefix)
+;;    'git-msg-prefix)
 
 ;; Configure
 
 ;; There are 3 variables to configure:
 
-;; - ~commit-msg-prefix-log-command~: defaults to "git log
+;; - ~git-msg-prefix-log-command~: defaults to "git log
 ;;   --pretty=format:\"%s\""
-;; - ~commit-msg-prefix-log-flags~: defaults to ""
+;; - ~git-msg-prefix-log-flags~: defaults to ""
 
-;; - ~commit-msg-prefix-regex~: defaults to  "^\\([^ ]*\\) "
+;; - ~git-msg-prefix-regex~: defaults to  "^\\([^ ]*\\) "
 
-;;  - ~commit-msg-prefix-input-method~: defaults to
+;;  - ~git-msg-prefix-input-method~: defaults to
 ;;    ido-completing-read. Change it to your favourite input
 ;;    method. ('completing-read 'ido-completing-read
 ;;    'helm-comp-read 'ivy-read)
@@ -77,65 +77,65 @@
 (require 's)
 (require 'dash)
 
-(defgroup commit-msg-prefix nil
+(defgroup git-msg-prefix nil
   "Pick a past commit message to build a new commit message."
-  :prefix "commit-msg-prefix-"
+  :prefix "git-msg-prefix-"
   :group 'applications)
 
-(defcustom commit-msg-prefix-log-command "git log --pretty=format:\"%s\""
+(defcustom git-msg-prefix-log-command "git log --pretty=format:\"%s\""
   "Main vcs command to run to populate the candidates list."
-  :group 'commit-msg-prefix
+  :group 'git-msg-prefix
   :type 'string)
 
-(defcustom commit-msg-prefix-log-flags ""
-  "Extra flags for `commit-msg-prefix-log-command'.
+(defcustom git-msg-prefix-log-flags ""
+  "Extra flags for `git-msg-prefix-log-command'.
 To narrow/extend the candidates listing.  For example:
 \"--author=rgrau --since=1.week.ago --no-merges\""
-  :group 'commit-msg-prefix
+  :group 'git-msg-prefix
   :type 'string)
 
-(defcustom commit-msg-prefix-regex "^\\([^ ]*\\)"
+(defcustom git-msg-prefix-regex "^\\([^ ]*\\)"
   "Regex to match against the populated list. The first match
 will be inserted on the current buffer"
-  :group 'commit-msg-prefix
+  :group 'git-msg-prefix
   :type 'string)
 
-(defcustom commit-msg-prefix-input-method 'completing-read
-  "Input method for ‘commit-msg-prefix’."
-  :group 'commit-msg-prefix
+(defcustom git-msg-prefix-input-method 'completing-read
+  "Input method for ‘git-msg-prefix’."
+  :group 'git-msg-prefix
   :type '(choice ('completing-read
                   'ido-completing-read
                   'helm-comp-read
                   'ivy-read)))
 
-(defvar commit-msg-prefix-prompt "pick commit:")
+(defvar git-msg-prefix-prompt "pick commit:")
 
-(defun commit-msg-prefix-input-fun ()
+(defun git-msg-prefix-input-fun ()
   "Show picker with candidates."
-  (funcall commit-msg-prefix-input-method
-           commit-msg-prefix-prompt
-           (commit-msg-prefix-1)))
+  (funcall git-msg-prefix-input-method
+           git-msg-prefix-prompt
+           (git-msg-prefix-1)))
 
-(defun commit-msg-prefix-1 ()
+(defun git-msg-prefix-1 ()
   "Internal function to fetch all candidates."
   (let ((vc-command (format "%s %s"
-                            commit-msg-prefix-log-command
-                            commit-msg-prefix-log-flags)))
+                            git-msg-prefix-log-command
+                            git-msg-prefix-log-flags)))
     (cons
      (s-prepend (car (vc-git-branches))
                 " - *current branch*")
      (s-lines (shell-command-to-string vc-command)))))
 
 ;;;###autoload
-(defun commit-msg-prefix ()
+(defun git-msg-prefix ()
   "Insert the relevant part of the chosen commit.
-Relevant meaning the result of `commit-msg-prefix-regex'
+Relevant meaning the result of `git-msg-prefix-regex'
 substitution."
   (interactive)
   (insert
    (second
-    (s-match commit-msg-prefix-regex
-             (commit-msg-prefix-input-fun)))))
+    (s-match git-msg-prefix-regex
+             (git-msg-prefix-input-fun)))))
 
-(provide 'commit-msg-prefix)
-;;; commit-msg-prefix.el ends here
+(provide 'git-msg-prefix)
+;;; git-msg-prefix.el ends here
